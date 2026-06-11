7ee3e375a4d278ffeaa7c5f1be8adab5c58b2010
LucianoFredes25 <luciano.fredes.c@mail.pucv.cl>
2026-06-10 22:36:39 -0400

    cambios

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://tramitessantodomingo-production.up.railway.app/api';
+const API_URL = 'https://tramitessantodomingo-production-5cb4.up.railway.app/api';
 
 export const authService = {
   register: async (userData: any) => {
